4427f0ce0aa847a5eab79a249c5709a97d7a2f27
Sachcith <sachcith@gmail.com>
2025-06-15 00:45:40 +0530

    Hello

--- app.py
+++ app.py
@@ -57,5 +57,5 @@ def answer():
         "links": links
     })
 
-if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+PORT = int(os.environ.get("PORT", 5000))
+app.run(host="0.0.0.0", port=PORT, debug=True)
